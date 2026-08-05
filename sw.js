ad72f3d3c9ee15a0cfe90cd8a046e44e07fdb04d
OmarLoay-iq <ommarloay.4@gmail.com>
2026-08-05 06:42:11 +0400

    deploy: recover Safari from black screen

--- sw.js
+++ sw.js
@@ -1,24 +1,13 @@
-self.LALA_RELEASE = "2026-08-05-login-fix-1";
-
-self.addEventListener("install", () => {
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     (async () => {
-      await self.clients.claim();
-
-      const windows = await self.clients.matchAll({ type: "window" });
-      await Promise.all(
-        windows.map((client) => {
-          const url = new URL(client.url);
-          if (url.origin !== self.location.origin) return undefined;
-
-          url.searchParams.set("release", self.LALA_RELEASE);
-          return client.navigate(url.href);
-        }),
-      );
+      const cacheNames = await caches.keys();
+      await Promise.all(cacheNames.map((name) => caches.delete(name)));
+      await self.registration.unregister();
     })(),
   );
 });
